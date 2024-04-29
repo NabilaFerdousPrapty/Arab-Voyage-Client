d1075f9295ddab49637b10348b37dfa4f4f0a21f
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-04-29 21:07:10 +0600

    update spots on going

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import UseAuth from "../../hooks/UseAuth";
+import { Link } from "react-router-dom";
 
 const MyList = () => {
   const { user } = UseAuth() || {};
@@ -16,20 +17,48 @@ const MyList = () => {
   return (
     <div>
       <section className="border-2  rounded-2xl my-7">
-	<div className="bg-blue-700 rounded-2xl ">
-		<div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900  ">
-			<h1 className="lg:text-5xl text-2xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-50">Discover the Enchantment of the Middle East with Arab Voyage</h1>
-			<p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-50">Embark on unforgettable journeys through vibrant cultures, breathtaking landscapes, and ancient wonders. Arab Voyage curates authentic travel experiences across the Middle East !</p>
-			<div className="flex flex-wrap justify-center">
-				<button type="button" className="px-8 py-3 m-2 text-lg font-semibold rounded border-gray-300 text-gray-900 border-2">Explore Destinations</button>
-				<button type="button" className="px-8 py-3 m-2 text-lg border rounded border-gray-300 text-gray-50">Plan Your Trip</button>
-			</div>
-		</div>
-	</div>
-	<img src="https://i.ibb.co/rMLdcjL/iran-travel-shazdeh-garden-kerman.jpg" alt="" className="w-5/6 mx-auto mb-12 -mt-20 bg-gray-500 rounded-lg shadow-md lg:-mt-40 " />
-</section>
+        <div className="bg-blue-700 rounded-2xl ">
+          <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900  ">
+            <h1 className="lg:text-5xl text-2xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-50">
+              Discover the Enchantment of the Middle East with Arab Voyage
+            </h1>
+            <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-50">
+              Embark on unforgettable journeys through vibrant cultures,
+              breathtaking landscapes, and ancient wonders. Arab Voyage curates
+              authentic travel experiences across the Middle East !
+            </p>
+            <div className="flex flex-wrap justify-center">
+              <button
+                type="button"
+                className="px-8 py-3 m-2 text-lg font-semibold rounded border-gray-300 text-gray-900 border-2"
+              >
+                Explore Destinations
+              </button>
+              <button
+                type="button"
+                className="px-8 py-3 m-2 text-lg border rounded border-gray-300 text-gray-50"
+              >
+                Plan Your Trip
+              </button>
+            </div>
+          </div>
+        </div>
+        <img
+          src="https://i.ibb.co/rMLdcjL/iran-travel-shazdeh-garden-kerman.jpg"
+          alt=""
+          className="w-5/6 mx-auto mb-12 -mt-20 bg-gray-500 rounded-lg shadow-md lg:-mt-40 "
+        />
+      </section>
+   {
+    items.length === 0 ? (
+      <p className="text-center text-gray-500">
+        You haven't added any tourist spots to your list yet. Start exploring and create your travel wishlist!
+      </p>
+    ) : (
       <div className="container p-2 mx-auto sm:p-4 ">
-        <h2 className="mb-4 text-2xl font-semibold leading-tight text-blue-500">Contacts</h2>
+        <h2 className="mb-4 text-2xl font-semibold leading-tight text-blue-500">
+          Contacts
+        </h2>
         <div className="overflow-x-auto">
           <table className="w-full p-6 text-xs text-left whitespace-nowrap">
             <colgroup>
@@ -48,23 +77,19 @@ const MyList = () => {
                 <th className="p-3">Country</th>
                 <th className="p-3">Location</th>
                 <th className="p-3">Total Visitor Per Year</th>
-                <th className="p-3">
-                Edit
-                </th>
-                <th className="p-3">
-              Delete
-                </th>
+                <th className="p-3">Edit</th>
+                <th className="p-3">Delete</th>
               </tr>
             </thead>
 
-            {items.map((item,index) => (
+            {items.map((item, index) => (
               <tbody
                 key={item._id}
                 item={item}
                 className="border-b  border-gray-300"
               >
                 <tr>
-                  <td className="px-3 text-2xl font-medium ">{index+1}</td>
+                  <td className="px-3 text-2xl font-medium ">{index + 1}</td>
                   <td className="px-3 py-2">
                     <p>{item.spotName}</p>
                   </td>
@@ -78,18 +103,27 @@ const MyList = () => {
                     <p>{item.totalVisitorsPerYear}</p>
                   </td>
                   <td className="">
-                    <div className="rounded-lg btn bg-sky-400 text-white">Update</div>
+                    <Link
+                      to={`/touristSpotDetails/${item._id}`}
+                      className="rounded-lg btn bg-sky-400 text-white"
+                    >
+                      Update
+                    </Link>
                   </td>
                   <td className="">
-                    <div className="rounded-lg btn bg-blue-400 text-white">Delete</div>
+                    <Link className="rounded-lg btn bg-blue-400 text-white">
+                      Delete
+                    </Link>
                   </td>
                 </tr>
-                
               </tbody>
             ))}
           </table>
         </div>
       </div>
+    )
+   }
+      
     </div>
   );
 };
--- src/components/TouristSpotDetails/TouristSpotDetails.jsx
+++ src/components/TouristSpotDetails/TouristSpotDetails.jsx
@@ -1,11 +1,323 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+
 
 const TouristSpotDetails = () => {
-    return (
-        <div>
-            
+  const [hide, setHidden] = useState(true);
+  const { id } = useParams();
+  console.log(id);
+  const [spot, setSpot] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/singleSpot/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSpot(data);
+        console.log(data);
+      });
+  }, [id]);
+  const toggleFormVisibility = () => {
+    setHidden(!hide);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+    const image = data.get("image");
+    const spotName = data.get("spotName");
+    const countryName = data.get("countryName");
+    const locationName = data.get("locationName");
+    const shortDescription = data.get("shortDescription");
+    const average_cost = data.get("average_cost");
+    const season = data.get("season");
+    const travelTime = data.get("travelTime");
+    const totalVisitorsPerYear = data.get("totalVisitorsPerYear");
+    
+    const updateSpot = {
+      image,
+      spotName,
+      countryName,
+      locationName,
+      shortDescription,
+      average_cost,
+      season,
+      travelTime,
+      totalVisitorsPerYear,
+      
+    };
+    console.log(updateSpot);
+    fetch(`http://localhost:5000/updateSpot/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateSpot),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Congratulations!",
+            text: "Your tourist spot has been Updated successfully!",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+          form.reset();
+        } else {
+          Swal.fire({
+            title: "OOPS!",
+            text: "Your tourist spot has not been updated",
+            icon: "Error",
+            confirmButtonText: "Cool",
+          });
+          form.reset();
+        }
+      });
+  };
+  return (
+    <div>
+      <div className="">
+        <div className="max-w-xl mx-auto p-4 shadow-md  border-2 border-gray-400 rounded-2xl md:my-3 h-[630px]">
+          <div className="flex justify-between pb-4 border-bottom">
+            <div className="flex items-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="mb-0 capitalize F font-semibold"
+              >
+                {spot.spotName}
+              </a>
+            </div>
+            <p>
+              <span className="text-xl font-semibold text-gray-600">
+                {spot.countryName}
+              </span>
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <img
+                src={spot.image}
+                alt=""
+                className="block object-cover object-center w-full rounded-md h-72 "
+              />
+              <div className="flex items-center text-xs">
+                <IoLocationOutline /> {spot.locationName}
+              </div>
+              <div className="text-center">
+                <span className="text-sm font-semibold text-gray-600">
+                  Average Cost:{" "}
+                </span>
+                <span className="text-sm font-semibold text-gray-600">
+                  {spot.average_cost}
+                </span>
+              </div>
+              <div className="text-center">
+                <span className="text-sm font-semibold text-gray-600">
+                  Season:{" "}
+                </span>
+                {spot.season ? (
+                  <span className="text-sm font-semibold text-gray-600">
+                    {spot.season}
+                  </span>
+                ) : (
+                  <span className="text-sm font-semibold text-gray-600">
+                    Not available
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <div className="flex justify-between">
+                <div>
+                  <span className="text-sm font-semibold text-gray-600">
+                    Travel Time:{" "}
+                  </span>
+                  <span className="text-sm font-semibold text-gray-600">
+                    {spot.travelTime}
+                  </span>
+                </div>
+                <div>
+                  <span className="text-sm font-semibold text-gray-600">
+                    Total Visitors Per Year:{" "}
+                  </span>
+                  <span className="text-sm font-semibold text-gray-600">
+                    {spot.totalVisitorsPerYear}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <a rel="noopener noreferrer" href="#" className="block">
+                <h3 className="text-xl font-semibold text-indigo-600"></h3>
+              </a>
+              <p className="leading-snug text-gray-600">
+                {spot.shortDescription}
+                ........
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex justify-center items-center">
+        <div
+          onClick={toggleFormVisibility}
+          className="btn bg-gray-800 px-2 py-1 rounded-md text-white my-3 hover:bg-gray-500"
+        >
+          Update Tourist Spot
+        </div>
+      </div>
+      {!hide && (
+        <section className="p-6 border-2 border-gray-600 rounded-xl my-5">
+          <form
+            onSubmit={handleSubmit}
+            className="container flex flex-col mx-auto space-y-12"
+          >
+            <fieldset className=" gap-6 p-6 rounded-md shadow-sm">
+              <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                <div className="col-span-full">
+                  <label htmlFor="image" className="text-sm">
+                    Tourist Spot image
+                  </label>
+                  <input
+                    name="image"
+                    id="image"
+                    defaultValue={spot.image}
+                    type="url"
+                    placeholder="Add your spot image"
+                    className="h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full">
+                  <label htmlFor="spot name" className="text-sm">
+                    Tourist Spot name
+                  </label>
+                  <input
+                    name="spotName"
+                    defaultValue={spot.spotName}
+                    id="spotName"
+                    type="text"
+                    placeholder="Add your spot name"
+                    className="h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="firstname" className="text-sm">
+                    Country name
+                  </label>
+                  <input
+                    name="countryName"
+                    defaultValue={spot.countryName}
+                    id="countryName"
+                    type="text"
+                    placeholder="Country name"
+                    className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 h-10 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="lastname" className="text-sm">
+                    Location
+                  </label>
+                  <input
+                    name="locationName"
+                    defaultValue={spot.locationName}
+                    id="locationName"
+                    type="text"
+                    placeholder="Location name"
+                    className="h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full">
+                  <label htmlFor="short description" className="text-sm">
+                    short description
+                  </label>
+                  <input
+                    name="shortDescription"
+                    defaultValue={spot.shortDescription}
+                    id="shortDescription"
+                    type="text"
+                    placeholder="Enter a short description"
+                    className="h-20 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="email" className="text-sm">
+                    {" "}
+                    Average Cost
+                  </label>
+                  <input
+                    name="average_cost"
+                    id=" average_cost"
+                    type="text"
+                    defaultValue={spot.average_cost}
+                    placeholder=" average_cost"
+                    className=" h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="email" className="text-sm">
+                    Seasonality
+                  </label>
+                  <input
+                    name="season"
+                    defaultValue={spot.season}
+                    id="season"
+                    type="text"
+                    placeholder="season"
+                    className=" h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="email" className="text-sm">
+                    {" "}
+                    Travel Time
+                  </label>
+                  <input
+                    name="travelTime"
+                    defaultValue={spot.travelTime}
+                    id=" average_cost"
+                    type="text"
+                    placeholder=" average_cost"
+                    className=" h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="email" className="text-sm">
+                    Total Visitors Per Year
+                  </label>
+                  <input
+                    name="totalVisitorsPerYear"
+                    defaultValue={spot.totalVisitorsPerYear}
+                    id="totalVisitorsPerYear"
+                    type="text"
+                    placeholder=" total Visitors Per Year"
+                    className=" h-10 w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-indigo-600 border-gray-300"
+                    required
+                  />
+                </div>
+                
+              </div>
+            </fieldset>
+            <div className="flex justify-center">
+              <button type="submit" className="btn w-full bg-sky-300">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default TouristSpotDetails;
